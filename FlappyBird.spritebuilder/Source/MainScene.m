daf570c250384d46bec3fc361b93946493db9ed1
Mladen <mladen@cobeisfresh.com>
2014-10-30 11:56:50 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -172,7 +172,7 @@
     for (CCNode *cloud in _clouds) {
         // move the cloud
         cloud.position = ccp(cloud.position.x -
-                             (character.physicsBody.velocity.x * delta), cloud.position.y);
+                             (character.physicsBody.velocity.x * delta * 0.9), cloud.position.y);
         
         // if the left corner is one complete width off the screen,
         // move it to the right
